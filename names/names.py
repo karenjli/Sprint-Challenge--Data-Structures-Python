d33ee3fb11640cc06a4d71e574c2e3bd9e64ef49
karenjli <karenjy.li@gmail.com>
2020-01-31 11:05:24 -0600

    Name duplication problem completed

--- names/names.py
+++ names/names.py
@@ -11,14 +11,28 @@ names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
 duplicates = []
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+# Run time is 10.3 seconds
+# Need to reduce to 1 or less second
+# Double for loop is time consuming
+hash = {}
+
+# for name_1 in names_1:
+#     for name_2 in names_2:
+#         if name_1 == name_2:
+#             duplicates.append(name_1)
+
+for names in names_1:
+    hash[names] = True
+
+for names_in_two in names_2:
+    if names_in_two in hash:
+        duplicates.append(names_in_two)
+    else:
+        pass
 
 end_time = time.time()
-print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
-print (f"runtime: {end_time - start_time} seconds")
+print(f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
+print(f"runtime: {end_time - start_time} seconds")
 
 # ---------- Stretch Goal -----------
 # Python has built-in tools that allow for a very efficient approach to this problem
